6bc1dc10e4cd6977134a17e7e717e847050de620
Nicholas Jefferis <jefferns@mail.uc.edu>
2021-01-20 11:52:28 -0500

    left,right,up,down,diagonal. no collision

--- Game.py
+++ Game.py
@@ -47,7 +47,11 @@ user = Player(450, 350, 13, 30, 100, 5, 3)
 def redraw():
     for row in cave.sprites:
         for cell in row:
-            window.blit(pygame.image.load(cell.sprite), (cell.x * 15, cell.y * 15))
+            if user.x > cell.x * 15 - 8 and user.x < cell.x * 15 + 8 and user.y > cell.y * 15- 8 and user.y < cell.y * 15 + 8:
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        current_cell = cave.sprites[cell.y + i][cell.x + j]
+                        window.blit(pygame.image.load(current_cell.sprite), (current_cell.x * 15, current_cell.y * 15))
     window.blit(user.still_sprite, (user.x, user.y))
     pygame.display.update()
 
@@ -95,6 +99,9 @@ while menu:
 # Main loop
 ##################################
 clock = pygame.time.Clock()
+for row in cave.sprites:
+    for cell in row:
+        window.blit(pygame.image.load(cell.sprite), (cell.x * 15, cell.y * 15))
 while run:
     clock.tick(27)  # change based on number of sprites
 
@@ -105,51 +112,36 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        if user.x > screen_width // 2 + 3:
-            user.x -= user.speed
-        elif bg.x + user.speed < 0:
-            bg.x += user.speed
-        elif not bg.x + user.speed < 0 and user.x - user.speed > 0:
-            user.x -= user.speed
+        user.x -= user.speed
+
         user.left = True
         user.right = False
         user.up = False
         user.down = False
         user.standing = False
 
-    elif keys[pygame.K_d]:
-        if user.x < screen_width // 2 - 3:
-            user.x += user.speed
-        elif bg.x - user.speed > screen_width - bg.width:
-            bg.x -= user.speed
-        elif not bg.x - user.speed > screen_width - bg.width and user.x + user.width + user.speed < screen_width:
-            user.x += user.speed
+    if keys[pygame.K_d]:
+        user.x += user.speed
+
         user.left = True
         user.right = False
         user.up = False
         user.down = False
         user.standing = False
 
-    elif keys[pygame.K_s]:
-        if user.y < screen_height // 2 - 3:
-            user.y += user.speed
-        elif bg.y - user.speed + bg.height > screen_height:
-            bg.y -= user.speed
-        elif not bg.y - user.speed + bg.height > screen_height and user.y + user.speed + user.height < screen_height:
-            user.y += user.speed
+    if keys[pygame.K_s]:
+        user.y += user.speed
+
         user.left = True
         user.right = False
         user.up = False
         user.down = False
         user.standing = False
 
-    elif keys[pygame.K_w]:
-        if user.y > screen_height // 2 + 3:
-            user.y -= user.speed
-        elif bg.y + user.speed < 0:
-            bg.y += user.speed
-        elif not bg.y + user.speed < 0 and user.y - user.speed > 0:
-            user.y -= user.speed
+    if keys[pygame.K_w]:
+        user.y -= user.speed
+
+
         user.left = False
         user.right = False
         user.up = True
